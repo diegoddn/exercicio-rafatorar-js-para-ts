517ab1cc36b516ff867fa3940f1e48776064579e
diegoddn <dhiego.natividade@hotmail.com>
2026-04-02 15:44:32 -0300

    feat Initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+const name = "João";
+const age = 30;
+const isAdult = age >= 18;
+if (isAdult) {
+    console.log(`${name} é maior de idade.`);
+}
+else {
+    console.log(`${name} não é maior de idade.`);
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name: string = "João";
+const age: number = 30;
+const isAdult: boolean = age >= 18;
+
+if (isAdult) {
+  console.log(`${name} é maior de idade.`);
+} else {
+  console.log(`${name} não é maior de idade.`);
+}
